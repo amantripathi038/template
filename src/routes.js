957f078cc877ab1fec89180e3ba18b7f4940a7e6
Aman Tripathi <amancselover@gmail.com>
2023-05-06 19:09:37 +0530

    Loading on auto login

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // layouts
+import { CircularProgress } from '@mui/material';
+
 import DashboardLayout from './layouts/dashboard';
 
 import UserPage from './pages/UserPage';
@@ -15,40 +17,53 @@ import Investment from './pages/Investment'
 
 export default function Router() {
 
+  const [isLoading, setIsLoading] = useState(false)
   const getUser = async (token) => {
     try {
+      setIsLoading(true)
       await userService.getUser(token);
     } catch (error) {
       console.error(error);
     }
+    finally {
+      setIsLoading(false)
+    }
   }
-
   const token = localStorage.getItem('token') ? localStorage.getItem('token').slice(1, -1) : (sessionStorage.getItem('token') ? sessionStorage.getItem('token').slice(1, -1) : null);
 
   useEffect(() => {
-    if (token) getUser(token)
+    if (token) {
+      getUser(token)
+    }
   }, [token]);
 
   const user = useSelector((state) => state.user.user);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/dashboard/app" />} />
-      <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/dashboard/app" />} />
-      <Route path="/register" element={!user ? <RegisterPage /> : <Navigate to="/dashboard/app" />} />
+    <>
+      {isLoading && (
+        <div style={{ position: 'fixed', top: '15%', left: '50%' }}>
+          <CircularProgress color="success" />
+        </div>
+      )}
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard/app" />} />
+        <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/dashboard/app" />} />
+        <Route path="/register" element={!user ? <RegisterPage /> : <Navigate to="/dashboard/app" />} />
 
-      <Route
-        path="/dashboard"
-        element={user ? <DashboardLayout /> : <Navigate to="/login" />}
-      >
-        <Route path="app" element={<DashboardAppPage />} />
-        <Route path="user" element={<UserPage />} />
-        <Route path="analysis" element={<DeepAnalysis />} />
-        <Route path="investment" element={<Investment />} />
-      </Route>
+        <Route
+          path="/dashboard"
+          element={user ? <DashboardLayout /> : <Navigate to="/login" />}
+        >
+          <Route path="app" element={<DashboardAppPage />} />
+          <Route path="user" element={<UserPage />} />
+          <Route path="analysis" element={<DeepAnalysis />} />
+          <Route path="investment" element={<Investment />} />
+        </Route>
 
-      <Route path="/404" element={<Page404 />} />
-      <Route path="*" element={<Navigate to="/404" />} />
-    </Routes>
+        <Route path="/404" element={<Page404 />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+    </>
   );
 }
